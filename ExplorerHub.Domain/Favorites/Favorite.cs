09cdb6d85bed869e7b4f2130ce6972c19056a3bb
lonverceli <285892461@qq.com>
2021-08-30 19:56:46 +0800

    save

---
+++ ExplorerHub.Domain/Favorites/Favorite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ExplorerHub.Framework.Domain;
+
+namespace ExplorerHub.Domain.Favorites
+{
+    public class Favorite : AggregateRoot
+    {
+        public Favorite(Guid id, string name, string location, byte[] icon)
+        {
+            Id = id;
+            Name = name;
+            Location = location;
+            Icon = icon;
+        }
+        
+        public string Name { get; }
+
+        public string Location { get; }
+
+        public byte[] Icon { get; }
+    }
+}
---
+++ ExplorerHub.EfCore/Favorites/FavoriteDbContext.cs
@@ -0,0 +1,40 @@
+﻿using ExplorerHub.Domain.Favorites;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExplorerHub.EfCore.Favorites
+{
+    public class FavoriteDbContext : DbContext
+    {
+        public DbSet<Favorite> Favorites { get; set; }
+
+        public FavoriteDbContext(DbContextOptions<FavoriteDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Favorite>(builder =>
+            {
+                builder.HasKey(favorite => favorite.Id);
+                builder.Property(favorite => favorite.Id)
+                    .ValueGeneratedOnAdd();
+
+                builder.Property(favorite => favorite.Name)
+                    .HasMaxLength(256)
+                    .IsRequired();
+
+                builder.Property(favorite => favorite.Location)
+                    .HasMaxLength(256)
+                    .IsRequired();
+
+                builder.HasIndex(favorite => favorite.Name)
+                    .IsUnique();
+
+                builder.Property(favorite => favorite.Icon)
+                    .IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
